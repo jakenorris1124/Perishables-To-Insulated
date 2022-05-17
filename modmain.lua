4c1620fc3facedf3e4a9141dc12ca31003820b60
jakenorris1124 <jakenorris1124@gmail.com>
2022-05-17 19:45:42 -0400

    first commit

---
+++ modmain.lua
@@ -0,0 +1,13 @@
+--[[
+
+***************************************************************
+Created by: Emoinm
+Date: May 17, 2022
+Description: Mod that automatically moves perishables to an insulated pack if there is room upon picking up.
+***************************************************************
+
+]]
+
+function inventorypostinit(component,inst)
+    print("hello inventory init!")
+end
