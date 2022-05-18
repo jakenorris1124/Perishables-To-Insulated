76652073c19bfd7d90509ad93a4b321e12c1229c
jakenorris1124 <jakenorris1124@gmail.com>
2022-05-18 12:42:56 -0400

    Added api version to modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -2,8 +2,8 @@ name = "Perishables to Insulated Pack"
 description = "Upon picking up a perishable, it will automatically be placed into your insulated pack if there is room."
 author = "Emoinm"
 version = "1.0"
-
-
+api_version = 10
+restart_required = false
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
